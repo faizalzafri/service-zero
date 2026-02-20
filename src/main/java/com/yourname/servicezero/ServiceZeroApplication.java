93e2945a3e0575ac3f347b9b1dcdd689ed3e5db5
faizalzafri <zafri.faizal@outlook.com>
2026-02-20 13:17:13 +0000

    app ready

---
+++ src/main/java/com/yourname/servicezero/ServiceZeroApplication.java
@@ -0,0 +1,13 @@
+package com.yourname.servicezero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceZeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceZeroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yourname/servicezero/ServiceZeroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yourname.servicezero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceZeroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
